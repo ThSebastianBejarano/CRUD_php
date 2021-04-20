2ba3fa1e1032bd43537c99f6259d9160fdfd3d76
ThSebastianBejarano <djsob2020>
2021-04-20 10:23:17 -0500

    Prueba

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 echo "HOLA MUNDO";
+
  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>HOLA MUNDO</h1>
+   </body>
+ </html>
