791b197c8d0bf29788d3530ed3ff20c383a454f9
ThSebastianBejarano <djsob2020>
2021-04-20 11:20:49 -0500

    prueba

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<?php
-
- ?>
-
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title>prueba</title>
-   </head>
-   <body>
-
-   </body>
- </html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>HOLA MUNDO</h1>
+  </body>
+</html>
